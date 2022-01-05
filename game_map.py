99983545d06f5ba4a39418650406d75fdfc15c0a
Stoico <44750560+JesperStoico@users.noreply.github.com>
2022-01-05 20:21:17 +0100

    Added entity and tile control

---
+++ game_map.py
@@ -0,0 +1,18 @@
+import  tile_types
+import numpy as np # type: ignore
+
+from tcod.console import Console
+
+class GameMap:
+    def __init__(self, width: int, height: int) -> None:
+        self.width, self.height = width, height
+        self.tiles = np.full((width, height), fill_value=tile_types.floor, order="F")
+
+        self.tiles[30:33, 22] = tile_types.wall
+
+    def in_bounds(self, x: int, y: int) -> bool:
+        """Returns True if x and y are inside of the bounds of this map."""
+        return 0 <= x < self.width and 0 <= y < self.height
+
+    def render(self, console: Console) -> None:
+        console.tiles_rgb[0:self.width, 0:self.height] = self.tiles["dark"]
