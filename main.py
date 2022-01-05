2c9f37c26da3e61ce0ac9b9e484c69ff34b26dfc
Stoico <44750560+JesperStoico@users.noreply.github.com>
2022-01-05 17:15:31 +0100

    Init

---
+++ main.py
@@ -0,0 +1,45 @@
+import tcod
+
+from actions import EscapeAction, MovementAction
+from input_handlers import EventHandler
+
+def main() -> None:
+    screen_width: int = 80
+    screen_height: int = 50
+
+    player_x: int = int(screen_width / 2)
+    player_y: int = int(screen_height / 2)
+
+    tileset = tcod.tileset.load_tilesheet("dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD)
+
+    event_handler = EventHandler()
+
+    with tcod.context.new_terminal(
+        screen_width,
+        screen_height,
+        tileset=tileset,
+        title="Another roguelike, not like the others",
+        vsync=True,
+    ) as context:
+        root_console = tcod.Console(screen_width, screen_height, order="F")
+        while True:
+            root_console.print(x=player_x, y=player_y, string="@")
+
+            context.present(root_console)
+
+            root_console.clear()
+
+            for event in tcod.event.wait():
+                action = event_handler.dispatch(event)
+
+                if action is None:
+                    continue
+                
+                if isinstance(action, MovementAction):
+                    player_x += action.dx
+                    player_y += action.dy
+                elif isinstance(action, EscapeAction):
+                    raise SystemExit()
+
+if __name__ == "__main__":
+    main()
