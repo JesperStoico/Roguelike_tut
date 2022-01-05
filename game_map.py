d7834a4c63a965c247117c6a4d71cc1705c42fce
Stoico <44750560+JesperStoico@users.noreply.github.com>
2022-01-05 21:16:01 +0100

    Added a simple procedural map generator

--- game_map.py
+++ game_map.py
@@ -6,9 +6,7 @@ from tcod.console import Console
 class GameMap:
     def __init__(self, width: int, height: int) -> None:
         self.width, self.height = width, height
-        self.tiles = np.full((width, height), fill_value=tile_types.floor, order="F")
-
-        self.tiles[30:33, 22] = tile_types.wall
+        self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")        
 
     def in_bounds(self, x: int, y: int) -> bool:
         """Returns True if x and y are inside of the bounds of this map."""
